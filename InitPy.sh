0255a0efee05ca934d46dde7a139f7cdd54f002a
Anouk Martin <anouk.mrtn@gmail.com>
2023-09-09 12:56:30 +0200

    Restore InitPy.sh

--- InitPy.sh
+++ InitPy.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# On enregistre tous les logs dans log.out pour pouvoir déboguer
-# exec 3>&1 4>&2
-# trap 'exec 2>&4 1>&3' 0 1 2 3
-# exec 1>log.out 2>&1
-
-# Pareil pour les variables d'environnement
-env | sort > env_init.out
-
 # Installation de ffmpeg
 sudo apt update && sudo apt install -y ffmpeg
 
@@ -19,8 +11,4 @@ pip install -U openai-whisper
 
 # Restitution des droit à l'utilisateur du service
 chown -R ${USERNAME}:${GROUPNAME} ${HOME}
-# ls -al /home/onyxia # avant work a un groupe 1000
-
-
-# Environnement final (pour déboguage)
-env | sort > env_final.out
+ls -al /home/onyxia # avant work a un groupe 1000
