82df55c094c9d2577d7839fec2123ad5d2e461c7
anoukmartin <anouk.mrtn@gmail.com>
2023-10-02 17:11:55 +0000

    ajout installation automatique de Whisper

--- InitPy.sh
+++ InitPy.sh
@@ -3,6 +3,9 @@
 # Installation de ffmpeg
 sudo apt update && sudo apt install -y ffmpeg
 
+# Installation de WhisperOpenAI
+pip install -U openai-whisper
+
 # Copie du notebook de transcription
 wget https://raw.githubusercontent.com/anoukmartin/Transcription-Whisper-x-SSP-Cloud/main/Transcription_Whisper.ipynb
 
