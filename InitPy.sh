7ad2cfedfb5e4e841e1cceb02761d1e039455cd3
Anouk Martin <anouk.mrtn@gmail.com>
2023-11-29 14:56:03 +0100

    Ouverture du notebook automatique

--- InitPy.sh
+++ InitPy.sh
@@ -8,3 +8,7 @@ pip install -U openai-whisper
 
 # Copie du notebook de transcription
 wget https://raw.githubusercontent.com/anoukmartin/Transcription-Whisper-x-SSP-Cloud/main/Transcription_Whisper.ipynb
+
+# Ouverture du notebook 
+IPYNB_PATH=Transcription_Whisper.ipynb
+[ -z "$IPYNB_PATH" ] || echo "\nc.NotebookApp.default_url = '/tree/work/${IPYNB_PATH}'" >> /home/jovyan/.jupyter/jupyter_notebook_config.py
